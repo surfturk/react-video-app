bb249e92c460613cca347718f2b9671a433b941c
Berkley Brown <berkleydeane@gmail.com>
2020-10-29 14:11:15 -0500

    finshed like button, converted like class component to functional component

--- src/components/common/like.js
+++ src/components/common/like.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 
-class Like extends Component {
-  render() {
-    return <i class="fa fa-heart-o" aria-hidden="true"></i>;
-  }
-}
+//functional component
+
+const Like = () => {
+  let classes = "fa fa-heart";
+  if (!this.props.liked) classes += "-o";
+  return (
+    <i
+      onClick={this.props.onClick}
+      style={{ cursor: "pointer" }}
+      className={classes}
+      aria-hidden="true"
+    ></i>
+  );
+};
 
 export default Like;
